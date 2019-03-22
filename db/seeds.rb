fc74715aad33630740eb79eedb2e4db05a4da0ff
dcjones1 <dcejjones@gmail.com>
2019-03-22 11:27:33 -0400

    fixed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,28 +30,34 @@ User.create(
   bio: 'A bibliophile, curator of fine charcuterie, and philosopher of the modern era.'
 )
 
-Post.create(
+post1 = Post.create(
   title: 'Harry Potter and the Deathly Hallows: A Viewing Party!',
   content: 'Join us for a viewing party this week projected on the community center lawn. Refreshments and general discussion to follow.',
   img: 'https://secure.img1-fg.wfcdn.com/im/23411414/resize-h600-w600%5Ecompr-r85/4049/40493777/Harry+Potter+%27Book+Cover+-+Deathly+Hallows%27+Graphic+Art+Print.jpg',
   user_id: 1
 )
 
-Post.create(
+post2 = Post.create(
   title: 'The Little Voice in Our Heads',
   content: 'A discussion on how to better listen to the calming voice inside us all.',
   img: 'https://bbkphilosophysociety.files.wordpress.com/2014/06/6a010535ce1cf6970c0163018bb9c2970d.jpg',
   user_id: 2
 )
 
-Post.create(
+post3 = Post.create(
   title: "Cheese: An Exploration of God's Gift to Man",
   content: 'A sharing of cheese plates and your finest charcuterie. If you bring American cheese, you will be asked politely, yet firmly, to leave.',
   img: 'https://iambaker.net/wp-content/uploads/2018/02/DIY-Cheese-Tray-1.jpg',
   user_id: 2
 )
 
-Tag.create(name: 'General', post_tags_ids: [1, 2, 3])
-Tag.create(name: 'Events', post_tags_ids: [1, 2, 3])
-Tag.create(name: 'For sale', post_tags_ids: [3])
-Tag.create(name: 'Lost', post_tags_ids: [2])
+
+
+tag1 = Tag.create(name: 'General')
+tag2 = Tag.create(name: 'Events')
+tag3 = Tag.create(name: 'For sale')
+tag4 = Tag.create(name: 'Lost')
+
+post1.tags = [tag1, tag2]
+post2.tags = [tag2, tag3]
+post3.tags = [tag1, tag4]
