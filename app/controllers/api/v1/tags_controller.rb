0461363400346bc987a7571234c3bcff28c4d8a1
canikwe <c.anikwe@gmail.com>
2019-03-23 11:21:18 -0400

    add tag index route

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::TagsController < ApplicationController
 
+  def index
+    @tags = Tag.all
+    render json: @tags
+  end
+
   def create
     @tag = Tag.new(tag_params)
     if @tag.save
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, except: [:new, :edit]
       resources :posts, except: [:new, :edit]
-      resources :tags, only: [:create]
+      resources :tags, only: [:create, :index]
       resources :post_tags, only: [:create, :destroy]
     end
   end
